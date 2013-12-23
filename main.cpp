6ab86deb41c6cefdbc3c5427c174254de1fb0146
Michal Bartkowiak <michal.bartkowiak@imgtec.com>
2013-12-23 15:20:37 +0100

    Temporary experiments

--- main.cpp
+++ main.cpp
@@ -3,20 +3,36 @@
 
 int main(int argc, char *argv[])
 {
-    CXIndex index = clang_createIndex(0, 0);
-    CXTranslationUnit tu =
+    auto index = clang_createIndex(0, 0);
+    auto tu =
         clang_parseTranslationUnit(index, 0, argv, argc,
                                    0, 0, CXTranslationUnit_DetailedPreprocessingRecord);
 
-    // int a; std::cout << a << std::endl;
+    struct List { int a; };
+//     int sum(union List *L) { return 0; }
 
     auto n = clang_getNumDiagnostics(tu);
     for (auto i = 0u; i != n; ++i)
     {
-        CXDiagnostic diag = clang_getDiagnostic(tu, i);
-        CXString diagStr =
+        auto diag = clang_getDiagnostic(tu, i);
+        auto diagStr =
             clang_formatDiagnostic(diag, clang_defaultDiagnosticDisplayOptions());
         std::cout << clang_getCString(diagStr) << std::endl;
+
+        auto numFixIts = clang_getDiagnosticNumFixIts(diag);
+
+        for (auto j = 0u; j != numFixIts; ++j)
+        {
+            CXSourceRange replRange;
+            auto fixIt = clang_getDiagnosticFixIt(diag, j, &replRange);
+            unsigned line, column, offset;
+            clang_getSpellingLocation(clang_getRangeStart(replRange), 0, &line, &column, &offset);
+            std::cout << line << " " << column << " " << offset <<
+                clang_getCString(fixIt) << std::endl;
+
+            clang_disposeString(fixIt);
+        }
+
         clang_disposeString(diagStr);
     }
 
